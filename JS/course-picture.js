3a529ad1e4d330d5d555786c14b4c1c48c64f7db
donna60732 <donna60732@gmail.com>
2024-05-07 18:57:34 +0800

    第10次更新

--- JS/course-picture.js
+++ JS/course-picture.js
@@ -11,14 +11,13 @@ document.addEventListener("DOMContentLoaded", function(){
     const big_image_3=document.querySelector("#big_image_3");
     const big_image_4=document.querySelector("#big_image_4");
 
-    small_image_1.style.border="2px solid  #CDCFA7";
+    small_image_1.style.border="2px solid  #fff";//#A3383C
     big_image_1.style.display="block";
     big_image_2.style.display="none";
     big_image_3.style.display="none";
     big_image_4.style.display="none";
 
     small_image_1.addEventListener('click',function(e){
-
         big_image_1.style.display="block";
         big_image_2.style.display="none";
         big_image_3.style.display="none";
